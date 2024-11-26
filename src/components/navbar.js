52359f9055794f1b16454b0131072b5b605e3460
alexwen.creates@gmail.com <alexwen.creates@gmail.com>
2024-11-26 14:32:01 -0600

    Add hiring page.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -84,6 +84,14 @@ const Navbar = ({ sidebarText }) => {
           <Link
             className="uppercase text-xs py-1 w-full hover:text-gray-600 flex items-center justify-between"
             activeClassName="active underline"
+            to="/hiring-tools-standards"
+          >
+            Hiring Tools + Tips
+            
+          </Link>
+          <Link
+            className="uppercase text-xs py-1 w-full hover:text-gray-600 flex items-center justify-between"
+            activeClassName="active underline"
             to="/testimonials"
           >
             Testimonials
